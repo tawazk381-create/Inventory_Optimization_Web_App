7b15767c2c33e6a6bc165c97fa26b99efcb71d02
Tawanda <tawazk381@gmail.com>
2025-09-04 13:57:47 +0200

    Update public/index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 // File: public/index.php
 declare(strict_types=1);
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 // Show errors during development
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
