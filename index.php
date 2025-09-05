f2a5cdf1e97459ff062802572e7134b3b959c51d
Tawanda <tawazk381@gmail.com>
2025-09-05 06:21:46 +0200

    Update index.php, update public/.htaccess, and add root .htaccess

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 // File: index.php (project root)
+// InfinityFree root index just forwards all requests to /public/index.php
 declare(strict_types=1);
 
-// Always forward into public/
-chdir(__DIR__ . '/public');
 require __DIR__ . '/public/index.php';
